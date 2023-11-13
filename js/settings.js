44ba056730b4df4387be93be16115ddca2d39c27
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2023-11-14 00:53:05 +0100

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -3,13 +3,13 @@
 */
 var settings = {
     // Nom de la Radio.
-    'radio_name': 'CINEMUSIC Radio',
+    'radio_name': 'Hit Music Radio',
 
     // URL du flux, eg: https://www.radioking.com/play/IDRADIO
-    'url_streaming': 'https://www.radioking.com/play/cinemusicradia',
+    'url_streaming': '	http://wrd.spaceworks.ovh:8000/radio',
 
     //ID de la radio.
-    'radio_id': 'cinemusicradia',
+    'radio_id': 'HMRadio',
 
     // Chemin d'accès à la couverture par défaut lorsqu'il est impossible d'en trouver une.
     'default_cover_art': 'img/defaultArt.webp',
